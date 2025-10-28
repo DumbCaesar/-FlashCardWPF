629f398491d38f8985bf40dc5d72b2d2e40a0246
DumbCaesar <dumbcaesar94@gmail.com>
2025-10-28 08:23:57 +0100

    top nav styling

--- FlashCardWPF/ViewModel/CardViewModel.cs
+++ FlashCardWPF/ViewModel/CardViewModel.cs
@@ -1,18 +1,25 @@
-﻿using System;
+﻿using FlashCardWPF.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using FlashCardWPF.Model;
 
 namespace FlashCardWPF.ViewModel
 {
-    public class CardViewModel
+    public class CardViewModel : INotifyPropertyChanged
     {
         public Deck CurrentDeck { get;  }
         public CardViewModel(Deck deck)
         {
             CurrentDeck = deck;
         }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
+
