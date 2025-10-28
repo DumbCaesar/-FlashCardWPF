2ed20c7fcf1b4bd6b3af4d67cf3575f9f4ebac33
Nicolaj Hansen <nicolaj_95@outlook.com>
2025-10-28 09:22:57 +0100

    current card

--- FlashCardWPF/ViewModel/CardViewModel.cs
+++ FlashCardWPF/ViewModel/CardViewModel.cs
@@ -11,10 +11,39 @@ namespace FlashCardWPF.ViewModel
 {
     public class CardViewModel : INotifyPropertyChanged
     {
-        public Deck CurrentDeck { get;  }
+        public Deck CurrentDeck { get; }
+        public Deck ReviewDeck { get; set; }
+        public Card CurrentCard { get; set; }
+
         public CardViewModel(Deck deck)
         {
             CurrentDeck = deck;
+            ReviewDeck = CreateReviewDeck(CurrentDeck);
+            CurrentCard = SetNextCard(ReviewDeck);
+        }
+
+        public Deck CreateReviewDeck(Deck deck)
+        {
+            Deck newDeck = new Deck("test");
+            int newCardCounter = 0;
+            int i = 0;
+            while (i < deck.Cards.Count)
+            {
+                Card card = deck.Cards[i];
+                if (card.IsNew && newCardCounter < 10)
+                {
+                    newDeck.Cards.Add(card);
+                    newCardCounter++;
+                }
+                else if (card.NextReview > DateTime.Now) newDeck.Cards.Add(card);
+                i++;
+            }
+            return newDeck;
+        }
+
+        public Card SetNextCard(Deck deck)
+        {
+            return deck.Cards[0];
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
