6638387d281c8fb13a23962f71df0cb06f40bd0b
DumbCaesar <dumbcaesar94@gmail.com>
2025-10-28 11:10:59 +0100

    visibility toggle

---
+++ FlashCardWPF/View/BoolToInverseVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FlashCardWPF.View
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public static readonly BoolToVisibilityConverter Instance = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = value is bool b && b;
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility v)
+                return v == Visibility.Visible;
+            return false;
+        }
+    }
+
+    public class BoolToInverseVisibilityConverter : IValueConverter
+    {
+        public static readonly BoolToInverseVisibilityConverter Instance = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = value is bool b && b;
+            return flag ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility v)
+                return v != Visibility.Visible;
+            return true;
+        }
+    }
+}
