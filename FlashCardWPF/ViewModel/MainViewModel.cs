e58e06c460122c782668af6a88657f70749d901f
DumbCaesar <dumbcaesar94@gmail.com>
2025-10-29 11:36:33 +0100

    removal of unneeded imports

--- FlashCardWPF/ViewModel/MainViewModel.cs
+++ FlashCardWPF/ViewModel/MainViewModel.cs
@@ -1,15 +1,8 @@
-﻿using FlashCardWPF.Model;
-using FlashCardWPF.View;
-using System;
-using System.Collections.Generic;
+﻿using FlashCardWPF.View;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
 
 namespace FlashCardWPF.ViewModel 
