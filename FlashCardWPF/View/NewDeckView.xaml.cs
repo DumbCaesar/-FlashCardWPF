da381db1e7a127e3c2fad1708f0b278d77607e47
DumbCaesar <dumbcaesar94@gmail.com>
2025-10-29 12:17:16 +0100

    basic design

---
+++ FlashCardWPF/View/NewDeckView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlashCardWPF.View
+{
+    /// <summary>
+    /// Interaction logic for NewDeckView.xaml
+    /// </summary>
+    public partial class NewDeckView : Window
+    {
+        public NewDeckView()
+        {
+            InitializeComponent();
+        }
+    }
+}
