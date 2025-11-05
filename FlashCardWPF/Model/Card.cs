a3add6a997dcb7fd5c15960649dcd5ed7e464ed2
Nicolaj Hansen <nicolaj_95@outlook.com>
2025-11-05 09:57:52 +0100

    Add comments to card

--- FlashCardWPF/Model/Card.cs
+++ FlashCardWPF/Model/Card.cs
@@ -6,17 +6,24 @@ using System.Threading.Tasks;
 
 namespace FlashCardWPF.Model
 {
+    /// <summary>
+    /// Class used to represent a single flash card with data used for spaced repetition
+    /// </summary>
     public class Card
     {
-        public string? Front {  get; set; }
-        public string? Back { get; set; }
-        public string? DeckName { get; set; }
-        public int? Interval { get; set; }
-        public double? EaseFactor { get; set; }
+        public string? Front {  get; set; } // Card question
+        public string? Back { get; set; } // Card answer
+        public string? DeckName { get; set; } // Optional: Which deck the card belongs to
+        public int? Interval { get; set; } // Time between reviews. Used for SM2 algorithm
+        public double? EaseFactor { get; set; } // Card difficulty multiplier. Used for SM2 algorithm
 
-        public DateTime NextReview { get; set; } = DateTime.Now;
-        public bool IsNew { get; set; } = true;
+        public DateTime NextReview { get; set; } = DateTime.Now; // When the card should be reviewed next (default to now for new cards)
+        public bool IsNew { get; set; } = true; // Whether the user has reviewed the card before 
+
+        // Constructor used for deserialization
         public Card() { }
+
+        // Constructor for creating new cards
         public Card(string question, string answer, string? deckName = null)
         {
             Front = question;
